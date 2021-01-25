bfc16f3cffd900e37d3e4e19c573e7288a85954f
kebin010110 <benkikikavan@gmail.com>
2021-01-25 23:07:33 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/montero_imapp/PMscreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.montero_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PMscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_mscreen);
+    }
+}
